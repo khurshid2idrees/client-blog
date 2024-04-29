16b8b7375059451b898c07052057c6f014b02fcc
khurshid2idrees <umarkhurshid3@gmail.com>
2024-04-29 12:05:40 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='text-red-600'>
+      hello world
+    </div>
+    </>
+  );
+}
+
+export default App;
